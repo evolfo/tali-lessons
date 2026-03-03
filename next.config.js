a8fb3c47c2824c009e24fd57db2ee59f81583910
Angelo Spampinato <aspampinato@protonmail.com>
2026-03-03 15:06:44 -0800

    Fixing links on store page to images

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const nextConfig = {
 
   // Image optimization
   images: {
-    loader: 'custom',
-    loaderFile: './lib/netlifyImageLoader.js',
-    unoptimized: false,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
